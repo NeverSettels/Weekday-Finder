6ec7fcb1bde96305ca72a13a38b729e4f22f7f74
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-13 16:06:28 -0700

    return the day of the week to the user via console

--- WF/Models/WF.cs
+++ WF/Models/WF.cs
@@ -1,6 +1,23 @@
 using System;
+using System.Linq;
 
 namespace WF.Models
 {
-  public class Wee
+  public class Week
+  {
+    // 5/13/2020 
+    public static DayOfWeek GetWeekday(string date)
+    {
+      string[] dateArr = date.Split("/");
+      DateTime dt = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
+      //Console.WriteLine(dt.DayOfWeek);
+      return dt.DayOfWeek;
+    }
+
+    public static void Main()
+    {
+      DayOfWeek weekday = GetWeekday("5/13/2020");
+      Console.WriteLine(weekday);
+    }
+  }
 }
